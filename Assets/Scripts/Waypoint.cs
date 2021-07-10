79d9b8867e98ed78331be56f88575d75c912035d
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-07-10 22:56:19 +0200

    Adds basic tower prefab and instantiate on click

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour{
-    
+
+    [SerializeField] GameObject towerPrefab;
     [SerializeField] bool isPlaceable;
     void OnMouseDown(){
         if (isPlaceable){
-            Debug.Log(transform.name);
+            Instantiate(towerPrefab, this.transform);
             isPlaceable = false;
         }
     }
