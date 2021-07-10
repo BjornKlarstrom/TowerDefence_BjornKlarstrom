8b84cc3a2df17c420be5bb06ef18f021f6f1b015
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-07-10 20:39:03 +0200

    Adds isPlaceable tiles and a tower

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0.1f, 10.0f)] float speed = 1.0f;
 
     void Start(){
         StartCoroutine(MoveToNextWaypoint());
@@ -16,9 +17,11 @@ public class Mover : MonoBehaviour
             var startPosition = this.transform.position;
             var endPosition = waypoint.transform.position;
             var movedPercent = 0.0f;
+            
+            this.transform.LookAt(endPosition);
 
             while (movedPercent < 1.0f){
-                movedPercent += Time.deltaTime;
+                movedPercent += Time.deltaTime * speed;
                 this.transform.position = Vector3.Lerp(startPosition, endPosition, movedPercent);
                 yield return new WaitForEndOfFrame();
             }
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour
-{
+public class Waypoint : MonoBehaviour{
     
+    [SerializeField] bool isPlaceable;
+    void OnMouseDown(){
+        if (isPlaceable){
+            Debug.Log(transform.name);
+            isPlaceable = false;
+        }
+    }
 }
