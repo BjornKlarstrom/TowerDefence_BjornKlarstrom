7140ce3664cae25e74bd5ce7e3b46e2619e168ea
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-07-12 19:17:38 +0200

    Adds simple enemy mesh and script to kill it

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
-{
-    
+public class EnemyHealth : MonoBehaviour{
+    [SerializeField] int maxHealth = 5;
+    int currentHealth = 0;
+
+    void Start(){
+        this.currentHealth = this.maxHealth;
+        Debug.Log(currentHealth);
+    }
+
+    void OnParticleCollision(GameObject other){
+        HandleHit();
+    }
+
+    void HandleHit(){
+        currentHealth--;
+
+        if (currentHealth <= 0){
+            Destroy(this.gameObject);
+        }
+    }
 }
