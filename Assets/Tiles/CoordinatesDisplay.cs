d1a0b942b26583f560fa7ba13d107b82b2b739bf
BjornKlarstrom <71707050+BjornKlarstrom@users.noreply.github.com>
2021-07-19 11:04:14 +0200

    adds restruct to folders as types

---
+++ Assets/Tiles/CoordinatesDisplay.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+
+namespace Editor{
+    [ExecuteAlways]
+    [RequireComponent(typeof(TextMeshPro))]
+    public class CoordinatesDisplay : MonoBehaviour{
+
+        [SerializeField] Color defaultColor = Color.white;
+        [SerializeField] Color blockedColor = Color.grey;
+    
+        TextMeshPro coordinateText;
+        Vector2Int coordinates;
+        Vector3 position;
+        Waypoint waypoint;
+
+        void Awake(){
+            coordinateText = GetComponent<TextMeshPro>();
+            coordinateText.enabled = true;
+            this.waypoint = GetComponentInParent<Waypoint>();
+            Display();
+        }
+
+        void Update(){
+            if (!Application.isPlaying){
+                Display();
+                DisplayInHierarchy();
+            }
+            SetTextColor();
+            ToggleCoordinateText();
+        }
+
+        void ToggleCoordinateText(){
+            if (Input.GetKeyDown(KeyCode.C)){
+                coordinateText.enabled = !coordinateText.IsActive();
+            }
+        }
+
+        void SetTextColor(){
+            coordinateText.color = waypoint.IsPlaceable ? defaultColor : blockedColor;
+        }
+
+        void Display(){
+            position = this.transform.position;
+            coordinates.x = Mathf.RoundToInt(position.x / UnityEditor.EditorSnapSettings.move.x);
+            coordinates.y = Mathf.RoundToInt(position.z / UnityEditor.EditorSnapSettings.move.z);
+            coordinateText.text = coordinates.x + "," + coordinates.y;
+        }
+
+        void DisplayInHierarchy(){
+            this.transform.parent.name = coordinates.x + "," + coordinates.y;
+        }
+    }
+}
